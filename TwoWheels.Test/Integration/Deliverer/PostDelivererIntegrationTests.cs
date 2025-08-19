0457f4361b26a6bb8b0b383c380b8f1a6765153d
Guilherme Abe <esx.gabe@terceiros.mag.com.br>
2025-08-19 00:41:27 -0300

    feat(deliverer): criação de teste integrado

---
+++ TwoWheels.Test/Integration/Deliverer/PostDelivererIntegrationTests.cs
@@ -0,0 +1,103 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TwoWheels.Functions.Services.Api.Deliverer;
+using TwoWheels.Functions.Services.Api.Deliverer.Commands;
+
+namespace TwoWheels.Test.Integration.Deliverer;
+
+public class PostDelivererIntegrationTests : IntegrationTestBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<PostDeliverer> _logger;
+
+    public PostDelivererIntegrationTests()
+    {
+        _mediator = Scope.ServiceProvider.GetRequiredService<IMediator>();
+        _logger = Scope.ServiceProvider.GetRequiredService<ILogger<PostDeliverer>>();
+    }
+
+    [Fact]
+    public async Task PostDeliverer_Should_Create_Deliverer_Successfully()
+    {
+        // Arrange
+        var command = new CreateDelivererCommand
+        {
+            Id = "deliverer-test-001",
+            Name = "João da Silva",
+            Cnpj = "12345678901234",
+            CnhNumber = "12345678901",
+            CnhTypeString = "AB",
+            BirthDate = DateTime.Now.AddYears(-25)
+        };
+
+        // Act
+        var result = await _mediator.Send(command);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        var delivererInDb = await DbContext.Deliverers.FindAsync(command.Id);
+        delivererInDb.Should().NotBeNull();
+        delivererInDb!.Name.Should().Be(command.Name);
+        delivererInDb.Cnpj.Should().Be(command.Cnpj);
+        delivererInDb.CnhNumber.Should().Be(command.CnhNumber);
+    }
+
+    [Fact]
+    public async Task PostDeliverer_Should_Fail_With_Invalid_Data()
+    {
+        // Arrange
+        var command = new CreateDelivererCommand
+        {
+            Id = "", // Invalid - empty ID
+            Name = "João da Silva",
+            Cnpj = "123", // Invalid - wrong length
+            CnhNumber = "12345678901",
+            CnhTypeString = "AB",
+            BirthDate = DateTime.Now.AddYears(-25)
+        };
+
+        // Act
+        var result = await _mediator.Send(command);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        var delivererInDb = await DbContext.Deliverers.FindAsync(command.Id);
+        delivererInDb.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task PostDeliverer_Should_Fail_With_Duplicate_Cnpj()
+    {
+        // Arrange
+        var firstCommand = new CreateDelivererCommand
+        {
+            Id = "deliverer-001",
+            Name = "João da Silva",
+            Cnpj = "12345678901234",
+            CnhNumber = "12345678901",
+            CnhTypeString = "AB",
+            BirthDate = DateTime.Now.AddYears(-25)
+        };
+
+        await _mediator.Send(firstCommand);
+
+        var secondCommand = new CreateDelivererCommand
+        {
+            Id = "deliverer-002",
+            Name = "Maria dos Santos",
+            Cnpj = "12345678901234",
+            CnhNumber = "12345678902",
+            CnhTypeString = "A",
+            BirthDate = DateTime.Now.AddYears(-30)
+        };
+
+        // Act
+        var result = await _mediator.Send(secondCommand);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        var deliverersCount = DbContext.Deliverers.Count();
+        deliverersCount.Should().Be(1);
+    }
+}
---
+++ TwoWheels.Test/Integration/IntegrationTestBase.cs
@@ -0,0 +1,103 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TwoWheels.Functions.Infra.Repositories.Data;
+using TwoWheels.Functions.Infra.Repositories.Interfaces;
+using TwoWheels.Functions.Services.Events.Interfaces;
+using TwoWheels.Functions.Services.Events.Motorcycle.Models;
+using TwoWheels.Functions.Services.Storage.Interfaces;
+using TwoWheels.Functions.Infra.Repositories;
+using TwoWheels.Functions.Services.Api.Deliverer.Validators;
+using TwoWheels.Functions.Shared.Behaviors;
+
+namespace TwoWheels.Test.Integration;
+
+public abstract class IntegrationTestBase : IDisposable
+{
+    protected readonly IHost Host;
+    protected readonly IServiceScope Scope;
+    protected readonly AppDbContext DbContext;
+    private bool _disposed;
+
+    protected IntegrationTestBase()
+    {
+        Host = CreateHostBuilder().Build();
+        Scope = Host.Services.CreateScope();
+        DbContext = Scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        DbContext.Database.EnsureCreated();
+    }
+
+    private static IHostBuilder CreateHostBuilder()
+    {
+        return new HostBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseInMemoryDatabase($"TestDb_{Guid.NewGuid()}"));
+
+                services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(TwoWheels.Functions.Services.Api.Deliverer.Commands.CreateDelivererCommand).Assembly));
+
+                services.AddScoped<IEventPublisher, MockEventPublisher>();
+                services.AddScoped<IStorageService, MockStorageService>();
+
+                services.AddScoped<IMotorcycleRepository, MotorcycleRepository>();
+                services.AddScoped<IDelivererRepository, DelivererRepository>();
+                services.AddScoped<IRentalRepository, RentalRepository>();
+
+                services.AddValidatorsFromAssemblyContaining<CreateDelivererCommandValidator>();
+                services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+                services.AddLogging(builder => builder.AddConsole());
+            });
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                DbContext?.Dispose();
+                Scope?.Dispose();
+                Host?.Dispose();
+            }
+            _disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
+
+public class MockEventPublisher : IEventPublisher
+{
+    public Task PublishMotorcycleCreatedAsync(MotorcycleCreatedEvent motorcycleEvent)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class MockStorageService : IStorageService
+{
+    public Task<string> SaveImageAsync(string base64Image, string fileName, string[] allowedExtensions)
+    {
+        return Task.FromResult($"mock-url/{fileName}");
+    }
+
+    public bool DeleteImageAsync(string filePath)
+    {
+        return true;
+    }
+
+    public bool ValidateImageFormat(string base64Image, string[] allowedExtensions)
+    {
+        return true;
+    }
+}
